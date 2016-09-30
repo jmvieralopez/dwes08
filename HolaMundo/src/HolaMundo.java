34038d1904244f7377cb37387a6c80d91939ffbe
alumno <alumno@w701>
2016-09-30 08:54:20 +0200

    tercera version

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -5,7 +5,8 @@ public class HolaMundo {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
 		System.out.println("Estoy trabajando con la máquina original");
-		System.out.println("Estoy trabajando desde la máquina clonada");
+		System.out.println("Estoy trabajando con la máquina clonada");
+		System.out.println("al parecer hubo un error y no me deja hacer commit sin que haya detectado modificaciones.");
 	}
 
 }
