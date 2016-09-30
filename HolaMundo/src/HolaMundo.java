9ae5250f997428dcd374aa4173d769009f1ae443
alumno <alumno@w701>
2016-09-30 08:46:19 +0200

    tercera version

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
 		System.out.println("Estoy trabajando con la máquina original");
+		System.out.println("Estoy trabajando desde la máquina clonada");
 	}
 
 }
