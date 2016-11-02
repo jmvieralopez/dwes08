f6e25223a176f48a276f70b1143c0585ef92aa29
alumno <alumno@w701>
2016-11-02 09:27:44 +0100

    actualizacion Torres Hanoi

--- TorresHanoi/src/PartidaHanoi.java
+++ TorresHanoi/src/PartidaHanoi.java
@@ -1,3 +1,4 @@
+import java.util.Collection;
 
 public class PartidaHanoi {
 	private Collection postes;
@@ -16,11 +17,23 @@ public class PartidaHanoi {
 	}
 	
 	boolean haTerminado(){
-		
+		if(postes.get(0).isEmpty() && postes.get(1).isEmpty() && postes.get(2).contains()){
+			return true;
+		}else{
+			return false;
+		}
 	}
 	
 	void mover(int posteOrigen, int posteDestino){
 		
+		//Si el número de poste origen o destino es inválido (no es 0, 1 ó 2).
+		
+		//Si el poste origen está vacío y por tanto no es posible extraer ningún disco
+		
+		//Si  el  tamaño  del  disco  que  está  en  la  cima  del  poste  origen  es  mayor  que  el 
+		//tamaño  del  disco  que  está  en  la  cima  del  poste  destino.
+		//¡Vigila  el  caso particular  en  el  que  el  poste  de  destino  está  vacío!  En  ese  caso  siempre  se 
+		//puede insertar.
 	}
 	
 	void dibujar(){
--- TorresHanoi/src/Principal.java
+++ TorresHanoi/src/Principal.java
@@ -1,27 +1,42 @@
+import java.util.Collection;
+import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
 		System.out.println("***********************");
 		System.out.println("* LAS TORRES DE HANOI *");
 		System.out.println("***********************");
-	
-		System.out.println("Menú principal:");
-		System.out.println("1- Nueva partida");
-		System.out.println("2- Salir del programa");
-		
-		System.out.println("Escoge una opción:");
-		
-		System.out.println("Introduce el número de discos:");
-	
-		System.out.println("¿Qué deseas hacer?");
-		System.out.println("1- Realizar un movimiento");
-		System.out.println("2- Abandonar la partida");
-	
-		System.out.println("Número de poste de origen");
-	
-		System.out.println("Número de poste de destino");
+		int opcion;
+		do{
+			System.out.println("Menú principal:");
+			System.out.println("1- Nueva partida");
+			System.out.println("2- Salir del programa");
+			System.out.println();
+			System.out.print("Escoge una opción: ");
+			opcion = s.nextInt();
+			if(opcion == 1){
+				System.out.print("Introduce el número de discos: ");
+				int numeroDiscos = s.nextInt();
+				PartidaHanoi p = new PartidaHanoi(new Collection, numeroDiscos);
+				int opcionPartida;
+				do{
+					System.out.println("¿Qué deseas hacer?");
+					System.out.println("1- Realizar un movimiento");
+					System.out.println("2- Abandonar la partida");
+					opcionPartida = s.nextInt();
+					int posteOrigen, posteDestino;
+					if(opcionPartida == 1){
+						System.out.print("Número de poste de origen");
+						posteOrigen = s.nextInt();
+						System.out.print("Número de poste de destino");
+						posteDestino = s.nextInt();
+					}
+				}while(opcionPartida != 2);
+			}
+		}while(opcion != 2);
 	}
-		
+	
 }
