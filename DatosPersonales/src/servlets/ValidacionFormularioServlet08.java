851b8d63d6d5c4cd6fb81bf6c3b77b1de880bc60
a2smrm <a2smrm@A104PC08>
2016-12-05 13:36:57 +0100

    actualizacion validacion

--- DatosPersonales/src/servlets/ValidacionFormularioServlet08.java
+++ DatosPersonales/src/servlets/ValidacionFormularioServlet08.java
@@ -3,6 +3,7 @@ package servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.Map;
@@ -36,11 +37,12 @@ public class ValidacionFormularioServlet08 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
         out.println("<!DOCTYPE html>");
-        out.println("<html><head><title>MostrarVariables</title><meta charset='UTF-8'/></head>");
-		String pass = request.getParameter("contraseña");
+        out.println("<html><head><title>MostrarVariables</title><meta charset='UTF-8'/></head><body>");
+		String pass = request.getParameter("contrasena");
         String fecha = request.getParameter("fecha");
         String numfavorito = request.getParameter("numfavorito");
 
+        //nombres de los campos
 		Enumeration <String> campos = request.getParameterNames();
 		out.println("<h3>Nombres de los campos</h3><ul>");
 		while(campos.hasMoreElements()){
@@ -48,6 +50,7 @@ public class ValidacionFormularioServlet08 extends HttpServlet {
 		}
 		out.println("</ul>");
 
+		//pares campo-valor
 		Map<String, String[]> campovalor = request.getParameterMap();
 		out.println("<h3>Pares campo-valor</h3><ul>");
 		campovalor.forEach((parametro, valores)->{
@@ -57,19 +60,66 @@ public class ValidacionFormularioServlet08 extends HttpServlet {
 			}
 		});
 		out.println("</ul>");
-				
+		
+		//contraseña
+		if(pass.isEmpty() || pass == null){
+			out.println("<p>No hay contraseña</p>");
+		}else{
+			boolean tieneMayuscula = false;
+			boolean tieneNumero = false;
+			boolean tieneCaracterEspecial = false;
+			for(int i=0; i<pass.length(); i++){
+				char car = pass.charAt(i);
+				if(Character.isUpperCase(car)){
+					tieneMayuscula = true;
+				}
+				if(Character.isDigit(car)){
+					tieneNumero = true;
+				}
+				if(!Character.isAlphabetic(car) && !Character.isDigit(car)){
+					tieneCaracterEspecial = true;
+				}
+			}
+			out.println("<p>Procesando al Contraseña</p>"+pass);
+	
+			if(tieneCaracterEspecial && tieneMayuscula && tieneNumero){
+				out.println("<p>Contraseña</p>"+pass);
+			}else{
+				out.println("<p>le falta algún caracter requerido</p>");
+			}
+		}
+		//formateo y comprobacion fecha
 		if(fecha.isEmpty() || fecha == null){
 			out.println("<p>No hay fecha</p>");
 		}else{
 			SimpleDateFormat fechaFormulario = new SimpleDateFormat("yyyy-MM-dd");
 			try{
 				Date fechaDate = fechaFormulario.parse(fecha);
-				SimpleDateFormat fechaSalida = new SimpleDateFormat("dd/MM/yyyy");
-				out.println("<p>Fecha: "+fechaSalida.format(fechaDate)+"</p>");
+				Date today = Calendar.getInstance().getTime();
+				if(fechaDate.before(today) || fechaDate.equals(today)){
+					SimpleDateFormat fechaSalida = new SimpleDateFormat("dd/MM/yyyy");
+					out.println("<p>Fecha: "+fechaSalida.format(fechaDate)+"</p>");
+				}else{
+					out.println("<p>La fecha es posterior a la actual</p>");					
+				}
 			}catch (ParseException | java.text.ParseException e){
 				e.printStackTrace();
+				out.println("<p>Ha habido un error. Compruebe si ha escrito bien la fecha</p>");				
 			}
 		}
+		
+		//comprobacion numero favorito
+		if(fecha.isEmpty() || fecha == null){
+			out.println("<p>No hay numero favorito</p>");
+		}else{
+			try{
+				int numfav = Integer.parseInt(numfavorito);
+			}catch(NumberFormatException e){
+				e.printStackTrace();
+				out.println("<p>Ha habido un error. Compruebe si ha escrito bien el número</p>");
+			}
+		}
+		
 		out.println("</body></html>");
 	}
 
