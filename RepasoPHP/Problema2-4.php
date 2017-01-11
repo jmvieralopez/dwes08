55efd358a3711c46af51045349db2501d1983b33
alumno <alumno@w701>
2017-01-11 13:37:15 +0100

    Terminado

--- RepasoPHP/Problema2-4.php
+++ RepasoPHP/Problema2-4.php
@@ -2,24 +2,28 @@
 <body>
 <h2>Problema 3</h2>
 <?php
-$cnt = 0;
 if (! isset ( $_POST ['enviar'] )) {
 	// var_dump($_POST);
 	?>
 <form action="Problema2-4.php" method="post">
 		Introduce numero: <input type="number" name="x" />
+		<input type="hidden" name="cnt" value="0"/>
 		<input type="submit" name="enviar">
 	</form>
 	<?php
 } else {
 	$x = $_POST['x'];
+	$cnt = $_POST['cnt'];
+	echo gettype($x);
 	if($x > 0 && $x <= 50){
 		$existe = false;
 		$rutaArchivo = "files/numeros.txt";
 		$archivo = fopen($rutaArchivo, "r") or die("Imposible abrir el archivo");
 		while(!feof($archivo)) {
 			//posible error, vigilar
-			if(fgets($archivo) === $x){
+			$resultado = strcmp(fgets($archivo), $x."\n");
+			echo $resultado;
+			if($resultado == 0){
 				$existe = true;
 			}
 		}
@@ -32,6 +36,12 @@ if (! isset ( $_POST ['enviar'] )) {
 				echo " " . fgets($archivo);
 			}
 			fclose($archivo);
+			//vaciar archivo
+			$archivo = fopen($rutaArchivo, "w+") or die("Imposible abrir el archivo para escritura");
+			fwrite($archivo,"");
+			fclose($archivo);
+			$cnt = 0;
+			echo "<h2>¡Juega otra vez!</h2>";
 		}else{
 			//probar con a+ si sobreescribe
 			$archivo = fopen($rutaArchivo, "a+") or die("Imposible abrir el archivo para escritura");
@@ -43,6 +53,7 @@ if (! isset ( $_POST ['enviar'] )) {
 	}
 	echo '<form action="Problema2-4.php" method="post">
 		Introduce numero: <input type="number" name="x" />
+		<input type="hidden" name="cnt" value="'.$cnt.'"/>
 		<input type="submit" name="enviar"></form>';
 }
 ?>
