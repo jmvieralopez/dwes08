5730919bc7c2188da88294fa57191bca13905f6f
jmvieralopez <jmvieralopez@gmail.com>
2017-01-18 10:26:35 +0100

    hecho

--- Java08-Archivos/src/GestorNombresServlet.java
+++ Java08-Archivos/src/GestorNombresServlet.java
@@ -10,6 +10,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -41,14 +43,18 @@ public class GestorNombresServlet extends HttpServlet {
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		PrintWriter out = response.getWriter();
 		ServletContext contexto = getServletContext();
+		response.setContentType("text/html");
 		String ruta = contexto.getRealPath("/files/nombres.txt");
+		//out.print(contexto.getRealPath("/files/nombres.txt"));
 		String nombre = request.getParameter("nombre");
-		readFile(ruta, out);
 		if(request.getParameter("enviar") != null) {
 			writeNewName(ruta, nombre);
 		}else if(request.getParameter("borrar") != null) {
-			out.write("Borrar");
+			deleteName(ruta, nombre);
 		}
+		readFile(ruta, out);
+		out.println("<h3>Añada más nombres</h3>");
+		out.println("<form action='./GestorNombresServlet' method='post'><input type='text' name='nombre'><input type='submit' name='enviar' value='enviar'><input type='submit' name='borrar' value='borrar'></form>");
 	}
 	
 	// LECTURA DE FICHERO
@@ -68,17 +74,22 @@ public class GestorNombresServlet extends HttpServlet {
 	}
 	
 	public void deleteName(String ruta, String nameToDelete) throws IOException{
+		ArrayList lineas = new ArrayList<String>();
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(ruta), "UTF-8"));
 		String linea;
 		while ((linea = br.readLine()) != null){
-			if(linea.equals(nameToDelete)){
-				
+			if(!linea.equals(nameToDelete)){
+				lineas.add(linea);
 			}
 		}
 			//out.println(linea);
 		br.close();
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(ruta, true), "UTF-8"));
-		//bw.write("");
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(ruta, false), "UTF-8"));
+		Iterator<String> it = lineas.iterator();
+		while(it.hasNext()) {
+			String nombre = it.next();
+			bw.write(nombre+"\n");
+		}
 		bw.close();
 	}
 
